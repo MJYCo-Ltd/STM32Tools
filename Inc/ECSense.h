183eb13a40cb37ff499d7b9b45ff3ba7fb89d2d0
ytyisme <wshyshb@gmail.com>
2025-12-08 23:19:42 +0800

    增加爱氪森传感器驱动

---
+++ Inc/ECSense.h
@@ -0,0 +1,14 @@
+/*
+ ******************************************************************************
+ * @file           : ECSense.h
+ * @brief          : Header for ECSense.c file.
+ *                   此文件为爱氪森传感器的驱动
+ ******************************************************************************
+ *
+ *  Created on: Dec 8, 2025
+ *      Author: yty
+ */
+#ifndef __YTY_ECSENSE_H_
+#define __YTY_ECSENSE_H_
+
+#endif//__YTY_ECSENSE_H_
---
+++ Src/ECSense.c
@@ -0,0 +1,7 @@
+/*
+ * ECSense.c
+ *
+ *  Created on: Dec 8, 2025
+ *      Author: yty
+ */
+#include "ECSense.h"
